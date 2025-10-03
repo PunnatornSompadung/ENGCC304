7d2e329c70fb4cccf613fa7e6a3a090e68218d19
Noorth <38935.prc@gmail.com>
2025-10-03 16:53:48 +0700

    Add files via upload

---
+++ Tum workss/Lab1.1.C
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main() {
+    int a, b, c, plus ; //input
+
+    scanf( "%d %d %d", &a, &b, &c ) ; //&string
+    plus = a + b + c ;
+    printf( "Answer = %d\n", plus ) ; //print/output
+    
+    return 0 ;
+} 
